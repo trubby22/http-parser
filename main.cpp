88bf9bdef6f988ad39b4b147b897738d6be3235a
trubby22 <piopio555888@gmail.com>
2024-08-20 12:23:24 +0200

    lines indices stack

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ constexpr int MAX_LINES = 15;
 constexpr int MAX_LENGTH = 300;
 constexpr int MAX_HEADERS_SIZE = 50;
 constexpr int MAX_BODY_SIZE = 50;
-
+constexpr int MAX_REQUEST_SIZE = 4'000;
 
 constexpr char request[] = "POST /submit-form HTTP/1.1\n"
     "Host: www.example.com\n"
@@ -45,6 +45,33 @@ constexpr char request[] = "POST /submit-form HTTP/1.1\n"
     "__utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; "                                                             \
     "__utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n"             \
     "\r\n"
+
+void print_raw(const string& str) {
+    for (char c : str) {
+        switch (c) {
+            case '\n': cout << "\\n"; break;
+            case '\t': cout << "\\t"; break;
+            case '\\': cout << "\\\\"; break;
+            case '\"': cout << "\\\""; break;
+            case '\r': cout << "\\r"; break;
+            default: cout << c; break;
+        }
+    }
+}
+
+void print_raw(const char *str, int start_ix, int length) {
+    for (int i = start_ix; i < start_ix + length; i++) {
+        char c = str[i];
+        switch (c) {
+            case '\n': cout << "\\n"; break;
+            case '\t': cout << "\\t"; break;
+            case '\\': cout << "\\\\"; break;
+            case '\"': cout << "\\\""; break;
+            case '\r': cout << "\\r"; break;
+            default: cout << c; break;
+        }
+    }
+}
 
 class HttpParser {
 public:
@@ -399,38 +426,25 @@ void parse_http_request_very_simple(string text) {
     }
 }
 
-void split_lines(const char* text, char lines[MAX_STRINGS][MAX_LENGTH]) {
-    int line_count = 0;
-    int line_length = 0;
-
+int split_lines(const char* text, int newlines[MAX_LINES]) {
+    int newline_count = 0;
+    int i = 0;
     const char* t = text;
 
-    while (*t != '\0' && line_count < MAX_LINES) {
-        if (*t == '\n' || line_length >= MAX_LENGTH - 1) {
-            // Null-terminate the current line
-            lines[line_count][line_length] = '\0';
-            line_count++;
-            line_length = 0;
-            if (*t == '\n') {
-                ++t; // Skip the newline character
-            }
-        } else {
-            // Copy the character to the current line
-            lines[line_count][line_length] = *t;
-            ++line_length;
-            ++t;
+    while (*(t + i) != '\0' && newline_count < MAX_LINES) {
+        if (*(t + i) == '\n') {
+            newlines[newline_count] = i;
+            newline_count++;
         }
+        i++;
     }
 
-    // Null-terminate the last line if there is space
-    if (line_length < MAX_LENGTH && line_count < MAX_LINES) {
-        lines[line_count][line_length] = '\0';
-    }
+    return newline_count;
 }
 
 void parse_http_request_stack_copy(const char *text) {
     char lines[MAX_STRINGS][MAX_LENGTH] = {0};
-    split_lines(text, lines);
+    // split_lines(text, lines);
 
     int blank_ix = MAX_LENGTH;
     for (int i = 0; i < MAX_LINES; ++i) {
@@ -486,29 +500,32 @@ void parse_http_request_stack_copy(const char *text) {
 }
 
 void parse_http_request_stack_indices(const char *text) {
-    char lines[MAX_STRINGS][MAX_LENGTH] = {0};
-    split_lines(text, lines);
+    // char lines[MAX_STRINGS][MAX_LENGTH] = {0};
+    int newlines[MAX_LINES] = {0};
+    int newline_count = split_lines(text, newlines);
 
-    int blank_ix = MAX_LENGTH;
-    for (int i = 0; i < MAX_LINES; ++i) {
-        if (lines[i][0] == '\0') {
-            blank_ix = min(blank_ix, i);
-        }
-    }
 
-    int snd_blank_ix = MAX_LENGTH;
-    for (int i = 0; i < MAX_LINES; ++i) {
-        if (lines[i][0] == '\0' && i > blank_ix) {
-            snd_blank_ix = min(snd_blank_ix, i);
-        }
-    }
 
-    cout << "blank_ix " << blank_ix << endl;
-    cout << "snd_blank_ix " << snd_blank_ix << endl;
+    // int blank_ix = MAX_LENGTH;
+    // for (int i = 0; i < MAX_LINES; ++i) {
+    //     if (lines[i][0] == '\0') {
+    //         blank_ix = min(blank_ix, i);
+    //     }
+    // }
 
-    for (int i = 0; i < MAX_LINES; ++i) {
-        cout << "Line " << i << " : " << lines[i] << endl;
-    }
+    // int snd_blank_ix = MAX_LENGTH;
+    // for (int i = 0; i < MAX_LINES; ++i) {
+    //     if (lines[i][0] == '\0' && i > blank_ix) {
+    //         snd_blank_ix = min(snd_blank_ix, i);
+    //     }
+    // }
+
+    // cout << "blank_ix " << blank_ix << endl;
+    // cout << "snd_blank_ix " << snd_blank_ix << endl;
+
+    // for (int i = 0; i < MAX_LINES; ++i) {
+    //     cout << "Line " << i << " : " << lines[i] << endl;
+    // }
 
     // Location start_line_loc;
     // Location headers_loc;
@@ -519,19 +536,91 @@ void parse_http_request_stack_indices(const char *text) {
     // headers_loc.start_ix = blank_ix
 }
 
+string get_substring(const char *text, int start_ix, int end_ix) {
+    string result;
+    
+    if (start_ix >= 0 && end_ix >= start_ix) {
+        for (int i = start_ix; i <= end_ix && text[i] != '\0'; ++i) {
+            result += text[i];
+        }
+    }
+    
+    return result;
+}
+
+void copying_heap(string request) {
+    istringstream stream(request);
+    string line;
+    vector<string> lines;
+
+    int blank_ix = 0;
+    int i = 0;
+
+    while (getline(stream, line)) {
+        lines.push_back(line);
+    }
+}
+
+void indices_heap(const string &request) {
+    // cout << request << endl;
+    // cout << endl;
+
+    vector<int> indices;
+    indices.push_back(-1);
+    for (size_t i = 0; i < request.size(); ++i) {
+        if (request[i] == '\n') {
+            indices.push_back(i);
+        }
+    }
+    indices.push_back(request.size());
+
+    // for (int i = 0; i < indices.size() - 1; i++) {
+    //     int length = indices[i + 1] - indices[i] - 1;
+    //     print_raw(request.substr(indices[i] + 1, length));
+    //     cout << endl;
+    // }
+}
+
+void indices_stack(const char *request) {
+    // cout << request << endl;
+    // cout << endl;
+
+    int indices[MAX_LINES] = {0};
+    int j = 0;
+    indices[j++] = -1;
+    int i = 0;
+    while (i < MAX_REQUEST_SIZE && request[i] != '\0') {
+        if (request[i] == '\n') {
+            indices[j++] = i;
+        }
+        i++;
+    }
+    indices[j++] = i;
+
+    // for (i = 0; i < j - 1; i++) {
+    //     int length = indices[i + 1] - indices[i] - 1;
+    //     print_raw(request, indices[i] + 1, length);
+    //     cout << endl;
+    // }
+}
+
 void bench() {
     int num_iters = 10'000;
+    string str_request = string(request);
     for (int i = 0; i < num_iters; i++) {
         #pragma clang optimize off
-        parse_http_request_very_simple(REQ);
+        // parse_http_request_very_simple(REQ);
         // parse_http_request_stack(REQ);
+        // indices_heap(str_request);
+        indices_stack(request);
         #pragma clang optimize on
         __asm__ __volatile__("" ::: "memory");
     }
 }
 
 int main(int argc, const char * argv[]) {
-    // bench();
-    parse_http_request_stack_indices(request);
+    bench();
+    // parse_http_request_stack_indices(request);
+    // indices_stack(request);
     return 0;
 }
